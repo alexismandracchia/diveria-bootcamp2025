6fb60d825dd061c2eaac69ae3017ad437031de0a
Mendez-Gabriel <mendez.gabriel.d@hotmail.com>
2025-08-20 22:51:42 -0300

    feat: "Se crea botones generales"

--- group-villafañe-mendez/tp-final/src/components/buttons/Buttons.tsx
+++ group-villafañe-mendez/tp-final/src/components/buttons/Buttons.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Button, ButtonProps } from "flowbite-react";
+import type { ReactNode } from "react";
 
-interface GradientButtonProps extends ButtonProps {
-  children: React.ReactNode;
+interface CustomButtonProps extends ButtonProps {
+  children: ReactNode;
 }
 
-export function GradientButton({ children, ...props }: GradientButtonProps) {
+export function GradientButton({ children, ...props }: CustomButtonProps ) {
   return (
     <Button
       {...props}
@@ -19,6 +20,37 @@ export function GradientButton({ children, ...props }: GradientButtonProps) {
       focus:ring-blue-300 
       dark:focus:ring-blue-800 
       py-2.5"
+    >
+      {children}
+    </Button>
+  );
+}
+
+export function ShadowButton({ children, ...props }: CustomButtonProps ) {
+  return (
+    <Button
+      {...props}
+      className="
+      text-white 
+      bg-gradient-to-r 
+      from-blue-500 
+      via-blue-600 
+      to-blue-700 
+      hover:bg-gradient-to-br 
+      focus:ring-4 
+      focus:outline-none 
+      focus:ring-blue-300 
+      dark:focus:ring-blue-800 
+      shadow-lg 
+      shadow-blue-500/50 
+      dark:shadow-lg 
+      dark:shadow-blue-800/80 
+      font-medium rounded-lg 
+      text-sm px-5 
+      py-2.5 
+      text-center 
+      me-2 
+      mb-2"
     >
       {children}
     </Button>
